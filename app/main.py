931df00f7d213d20686a0df75675598434466ba0
cantoroda87 <cantorodao@gmail.com>
2025-05-03 14:43:56 +0200

    20250503 CORS Policy

--- app/main.py
+++ app/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 # Habilitar CORS (para conectar con React en localhost)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Cambia si usas otro puerto
+        allow_origins=[
+        "https://a-cistierna-frontend.vercel.app",  # Dominio de producción
+        "http://localhost:5173",                    # Dominio de desarrollo local
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
